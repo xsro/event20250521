1b17d80edf2aa973c39b8ba7717e2e1bdebc1172
Chasen Liu <xsro@foxmail.com>
2025-04-26 12:24:01 +0800

    add github ci

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
+    base: '/event20250521/',
+    plugins: [
+        tailwindcss(),
+    ],
 })
